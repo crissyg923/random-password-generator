dd1a88a83037d2af499d7fa4499132c8184fd270
Crystal Green <crissybrown1992@icloud.com>
2023-07-20 14:52:08 -0400

    Reverted to previous version to get rid of bug

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//Empty string and array variables for gathering user-chosen characters.
+var passwordCharacters= [];
 var guaranteePassword = [];
+var completedpassword= "";
 var userPasswordChar = "";
-
-//Arrays holding possible characters
 
 var lowercaselettersarray = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercaselettersarray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
