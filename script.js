599bb04cc5d1d8f8c1a02b79141040c39bcef780
Crystal Green <crissybrown1992@icloud.com>
2023-07-20 14:43:04 -0400

    Fixed code with tutor, added math random functions to each if statement, added a working for loop, and added a shuffle right before password is written

--- script.js
+++ script.js
@@ -1,43 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+//Empty string and array variables for gathering user-chosen characters.
+var guaranteePassword = [];
+var userPasswordChar = "";
+
+//Arrays holding possible characters
+
+var lowercaselettersarray = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaselettersarray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbersarray = [1,2,3,4,5,6,7,8,9,0];
+var specialcharactersarray = ['!','@','#','$','%','^','*','?'];
+
 var generatePassword= function(){
 
-  passwordLength=window.prompt("Password Length.  Please choose a number from 8 to 128.")
+  var passwordLength = parseInt(window.prompt("Password Length.  Please choose a number from 8 to 128.", "enter #"));
+  if(isNaN(passwordLength)) {
+    alert("Passsowrd must be a number")
+    return null;
+  }
   if (passwordLength < 8 || passwordLength > 128) {
-    return;
+    alert("Passsowrd must be withing requirement")
+    console.log(typeof passwordLength);
+    return null;
+  } else {
+    // if (passwordLength >7 || passwordLength <129) {
+      var lowercaseletters = window.confirm("would you like to include lowercase letters?");
+      var uppercareletters = window.confirm("Would you like to include uppercase letters?");
+      var numbers = window.confirm("Would you like to include numerical values?");
+      var specialcharacters = window.confirm("Would you like yo include special characters?");
+    // }
   }
-  if (passwordLength >7 || passwordLength <129) {
-    var lowercaseletters= window.confirm("would you like to include lowercase letters?");
-    var uppercareletters=window.confirm("Would you like to include uppercase letters?");
-    var numbers=window.confirm("Would you like to include numerical values?");
-    var specialcharacters=window.confirm("Would you like yo include special characters?");
-  }
-var passwordCharacters= [];
 
 if (lowercaseletters) {
-  var lowercaselettersarray =['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  passwordCharacters.unshift(lowercaselettersarray);
+  userPasswordChar += lowercaselettersarray.join("");
+  var getRandChar = Math.floor(Math.random() * lowercaselettersarray.length);
+  guaranteePassword.push(lowercaselettersarray[getRandChar]);
 }
 
 if (uppercareletters){
-  var uppercaselettersarray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  passwordCharacters.unshift(uppercaselettersarray);
+  userPasswordChar += uppercaselettersarray.join("");
+  var getRandChar = Math.floor(Math.random() * uppercaselettersarray.length);
+  guaranteePassword.push(uppercaselettersarray[getRandChar]);
 } 
 if (numbers){
-  var numbersarray = [1,2,3,4,5,6,7,8,9,0];
-  passwordCharacters.unshift(numbersarray);
+  userPasswordChar += numbersarray.join("");
+  var getRandChar = Math.floor(Math.random() * numbersarray.length);
+  guaranteePassword.push(numbersarray[getRandChar]);
 }
 if (specialcharacters) {
-  var specialcharactersarray = ['!','@','#','$','%','^','*','?'];
-  passwordCharacters.unshift(specialcharactersarray);
+  userPasswordChar += specialcharactersarray.join("");
+  var getRandChar = Math.floor(Math.random() * specialcharactersarray.length);
+  guaranteePassword.push(specialcharactersarray[getRandChar]);
 }
-for (var i=0; i<passwordLength; i++){
-  var index = Math.floor(Math.random() * passwordCharacters.length);
-  var finalpassword = passwordCharacters[index];
+console.log("before loop");
+console.log(passwordCharacters);
+console.log(completedpassword);
+
+for (let i = guaranteePassword.length; i < passwordLength; i++){ 
+  var index = Math.floor(Math.random() * userPasswordChar.length);
+  // completedpassword = userPasswordChar[index];
+  guaranteePassword.push(userPasswordChar[index])
 }
-return finalpassword;
+var result = shuffleArr(guaranteePassword)
+return result.join("");
 }
 
+function shuffleArr(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -46,7 +84,25 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+// var arr = ["D", "A", "B", "C"]
+
+// function getRand(x) {
+//   var randomizer = Math.floor(Math.random() * x.length);
+//   var x = arr[randomizer]
+//   console.log("INDEX: ", randomizer)
+//   console.log("CHAR: ", x)
+// }
+
+
+// getRand(arr)
