22fdf8b33eda4fc5fa76789a2d06a0ff0066a4fc
Crystal Green <crissybrown1992@icloud.com>
2023-07-20 15:38:29 -0400

    Added comments to Javascript file

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordCharacters= [];
+//Empty variable and strings to hold characters chosen by user.
 var guaranteePassword = [];
-var completedpassword= "";
 var userPasswordChar = "";
+
+//Arrays holding character choices for password.
 
 var lowercaselettersarray = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercaselettersarray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbersarray = [1,2,3,4,5,6,7,8,9,0];
 var specialcharactersarray = ['!','@','#','$','%','^','*','?'];
 
-var generatePassword= function(){
+//Function to generate password.
 
+var generatePassword= function(){
+//This will send a prompt to user to request a number for password length.
+//parseInt along with isNAN ensures that function will halt if non-integer character is inputted by user.
   var passwordLength = parseInt(window.prompt("Password Length.  Please choose a number from 8 to 128.", "enter #"));
   if(isNaN(passwordLength)) {
     alert("Passsowrd must be a number")
     return null;
   }
+  //Ensures that password is within parameters of 8 and 128
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Passsowrd must be withing requirement")
+    alert("Passsowrd must be within requirement")
     console.log(typeof passwordLength);
     return null;
+
   } else {
-    // if (passwordLength >7 || passwordLength <129) {
+    // if (passwordLength >7 || passwordLength <129) 
+    //These windows will gather the users choices of what characters to include in the random password.
       var lowercaseletters = window.confirm("would you like to include lowercase letters?");
       var uppercareletters = window.confirm("Would you like to include uppercase letters?");
       var numbers = window.confirm("Would you like to include numerical values?");
       var specialcharacters = window.confirm("Would you like yo include special characters?");
-    // }
+    
   }
 
+//If user chooses lowercase letters, the lowercaselettersarray is concatenated with userPasswordChar (the empty string holding 
+//the user chosen characters) and joined as a string.  The variable "getRandChar" then uses the math.floor and math.random
+//functions to grab a random character.  That random character is then added to the guaranteePassword empty array.  This process
+//is repeated for the following 3 if statements as well.
 if (lowercaseletters) {
   userPasswordChar += lowercaselettersarray.join("");
   var getRandChar = Math.floor(Math.random() * lowercaselettersarray.length);
@@ -51,15 +62,18 @@ if (specialcharacters) {
   var getRandChar = Math.floor(Math.random() * specialcharactersarray.length);
   guaranteePassword.push(specialcharactersarray[getRandChar]);
 }
-console.log("before loop");
-console.log(passwordCharacters);
-console.log(completedpassword);
+
+
+//This for loop takes the password characters we obtained so far and iterates through the chosen character arrays 
+// grabbing more random characters until we have the password length chosen by the user.
 
 for (let i = guaranteePassword.length; i < passwordLength; i++){ 
   var index = Math.floor(Math.random() * userPasswordChar.length);
-  // completedpassword = userPasswordChar[index];
   guaranteePassword.push(userPasswordChar[index])
 }
+//The following lines use the function "shuffleArr" to mix the characters up even more , ensuring that the password is uniquely
+//chosen every time. the result.join("") ensures that the password is a string.
+
 var result = shuffleArr(guaranteePassword)
 return result.join("");
 }
@@ -75,7 +89,8 @@ function shuffleArr(a) {
   return a;
 }
 
-// Write password to the #password input
+// This function writes the password to the screen for the user and states that the password is obtained from the 
+// generatePassword() function defined above.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -84,7 +99,7 @@ function writePassword() {
 
 
 }
-// Add event listener to generate button
+// This ensures the password is written after going through our prompts.
 generateBtn.addEventListener("click", writePassword);
 
 
@@ -93,14 +108,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// var arr = ["D", "A", "B", "C"]
-
-// function getRand(x) {
-//   var randomizer = Math.floor(Math.random() * x.length);
-//   var x = arr[randomizer]
-//   console.log("INDEX: ", randomizer)
-//   console.log("CHAR: ", x)
-// }
-
-
-// getRand(arr)
